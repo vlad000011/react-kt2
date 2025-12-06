b120c0962b465edd10c59eb3491b7be8135854b6
vlad000011 <105325266+vlad000011@users.noreply.github.com>
2025-12-06 11:49:05 +0300

    Create notFound.jsx

---
+++ src/notFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div style={{padding:20}}>
+      <h1>404 — Page not found</h1>
+      <p>Похоже, здесь ничего не найдено. Вернитесь назад.</p>
+    </div>
+  );
+}
